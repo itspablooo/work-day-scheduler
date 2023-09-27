c7148b6adc7b4684a0d7614d74dae9f1997cc6e2
Pablo Rivera <pablomrivera@outlook.com>
2023-09-26 21:21:37 -0700

    added divs for the missing time block. added js to set class to update dynamically based on time

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+// pull the current hour from day.js and set it to variable currentHour 
+  var currentHour = dayjs().hour()
+
+// when save button is clicked, run the function
+  $(".saveBtn").on("click", function () { 
+    
+    // set the value of the text area from sibling of the button clicked using $(this)
+    // value = value for local storage
+    var value = $(this).siblings(".description").val()
+    // time = key for local storage. will use parent element of the button clicked using $(this)
+    var time = $(this).parent().attr("id")
+    // set key and value in local storage
+    localStorage.setItem(time, value)
+  });
+
+  // this function will update class used for styling to update colors based on the time.
+  function updateHour() {
+    // select class time-block an each loop
+    $(".time-block").each(function() {
+      // parse id of each time-block div to retrieve integer as a number 
+      var blockHour = parseInt($(this).attr("id").split("-")[1])
+
+      // run an if else statement to decide which style class is added
+      // if block hour is less than current hour, apply past class style
+      if (blockHour < currentHour) {
+        $(this).addClass("past")
+        // if blockhour = current hour, remove past style and apply present class
+      } else if (blockHour == currentHour) {
+        $(this).removeClass("past").addClass("present")
+        // anything else will remove past and present class, and apply future class. 
+      } else {
+        $(this).removeClass("past").removeClass("present").addClass("future")
+      }
+    })
+  }
+  // call function update hour to update class styling
+  updateHour()
+
+  // set interval so the function is called  every 60000 milliseconds (1minute)
+  // this allows styling to be set based on time 
+  setInterval(updateHour, 60000)
+
+  // return value of storage items for each time-block
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 });
